6a95aa920db3ea6179ac6dbb732b161f61f04ead
Martin <zxm@basecity.com>
2012-03-19 16:13:00 +0800

    now can click api to see the document.

--- src/cljs/clj_rpc/help.cljs
+++ src/cljs/clj_rpc/help.cljs
@@ -1,18 +1,47 @@
-(ns web-command.core
+(ns clj-rpc.help
   (:require [goog.dom :as dom]
             [goog.events :as events]
             [clj-rpc.net :as net]
-            [domina :as domina]
+            [domina :as d]
+            [domina.css :as css]
+            [domina.events :as de]
             [clojure.browser.repl :as repl]))
 
+(defn box []
+  (str "<box>"))
+
 (def api-data (atom {}))
+
+(defn all-api-link []
+  (css/sel "#j_docList > li"))
+
+(defn find-doc [name]
+  (first (filter #(= (:name %) name) (apply concat (vals @api-data) ))))
+
+(defn fill-doc [name]
+  (let [doc (find-doc name)]
+    (d/set-text! (css/sel "#j_docContent > textarea") (get doc :doc))
+    (d/set-text! (css/sel "#j_argList > p") (:arglists doc))
+    (d/set-text! (css/sel "#j_options > p") (pr-str (:options doc)))))
+
+(defn add-api-click-event []
+  (let [nds (all-api-link)]
+    (de/listen! nds
+               "click"
+               (fn [e]
+                 (let [obj (.-currentTarget e)]
+                   (d/remove-class! nds "current")
+                   (d/add-class! obj "current")
+                   (fill-doc (d/text (css/sel obj "a") )))))))
 
 (defn fill-in-api [url data]
   (let [data (sort-by :name data)]
     (swap! api-data assoc url data)
-    (let [p (domina/by-id "j_docList")]
+    (let [p (d/by-id "j_docList")]
       (doall
-       (map #(domina/append! p (str "<li><a>" (:name %) "</a></li>")) data) ))))
+       (map #(d/append! p (str "<li id=><a href=\"javascript:;\">"
+                               (:name %) "</a></li>")) data) )
+      (add-api-click-event))))
 
 (defn error [data]
   (js/alert data))
@@ -24,7 +53,7 @@
 
 (defn start-app []
   (repl/connect "http://localhost:9000/repl")
-  (get-api "http://localhost:9876/clj/help"))
+  (get-api "http://newyear.volcano/clj/help"))
 
 (let [document (dom/getDocument)]
   (events/listen
@@ -33,4 +62,3 @@
    (fn []
      (when (= "complete" document/readyState)
        (start-app)))))
-
